f9456d4ea5c8b824674dd6b7b9c0b5a385bca5e4
rehalm <techrehalm@gmail.com>
2024-10-07 10:42:29 +0200

    1 commit

---
+++ components/header/Header.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+import React from 'react'
+import Menu from './Menu'
+
+const Header = () => {
+  return (
+    <header className='fixed top-0 w-full z-30'>
+      <nav>
+        <div className="navbar justify-between bg-base-300">
+          <div>
+            <label htmlFor="my-drawer" className="btn btn-square btn-ghost">
+              <button className='md:hidden'>
+                 <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-5 h-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+              </button>
+             
+            </label>
+            <Link href="/" className="btn btn-ghost text-lg">
+              Deby Hotel
+            </Link>
+          </div>
+
+          <Menu />
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ components/header/Menu.tsx
@@ -0,0 +1,85 @@
+'use client'
+import { signIn, signOut, useSession } from 'next-auth/react'
+
+import Link from 'next/link'
+const Menu = () => {
+
+  const signoutHandler = () => {
+    signOut({ callbackUrl: '/signin' })
+  }
+
+  const { data: session } = useSession()
+
+
+  const handleClick = () => {
+    ;(document.activeElement as HTMLElement).blur()
+  }
+
+  return (
+    <>
+      <div className="hidden md:block">
+      </div>
+      <div>
+        <ul className="flex items-stretch">
+          <Link href="/rooms" className='p-3 transition duration-500 hover:text-success hover:scale-125 hover:bg-gray-800 rounded-lg md:mx-3'>Find Rooms</Link>
+          <Link href="/bookings" className='p-3 transition duration-500 hover:text-success hover:scale-125 hover:bg-gray-800 rounded-lg md:mx-3'>Bookings</Link>
+          {session && session.user ? (
+            <>
+              <li>
+                <div className="dropdown dropdown-bottom dropdown-end">
+                  <label tabIndex={0} className="btn btn-ghost rounded-btn">
+                    {session.user.name}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                      />
+                    </svg>
+                  </label>
+                  <ul
+                    tabIndex={0}
+                    className="menu text-success dropdown-content z-[1] p-2 shadow bg-base-300 rounded-box w-52 "
+                  >
+                    {session.user.role === "admin" && (
+                      <li onClick={handleClick}>
+                        <Link href="/admin/">Admin Dashboard</Link>
+                      </li>
+                    )}
+
+                    <li onClick={handleClick}>
+                      <Link href="/order-history">Order history </Link>
+                    </li>
+                    <li onClick={handleClick}>
+                      <Link href="/profile">Profile</Link>
+                    </li>
+                    <li onClick={handleClick}>
+                      <button type="button" onClick={signoutHandler}>
+                        Sign out
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </>
+          ) : (
+            <>
+                  <Link href="/register" className='p-3 transition duration-500 hover:text-success hover:scale-125 hover:bg-gray-800 rounded-lg md:mx-3'>Register</Link>
+                  <Link href="/signin" className='p-3 transition duration-500 hover:text-success hover:scale-125 hover:bg-gray-800 rounded-lg md:mx-3'>Sign In</Link>
+
+            </>
+          )}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Menu
